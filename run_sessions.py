98ae8bb9e43d7b4a2f9f679193f0eaaa7210e908
Trace <teohrt18@gmail.com>
2020-07-18 23:07:59 -0700

    Modularity

--- run_sessions.py
+++ run_sessions.py
@@ -1,4 +1,3 @@
-
 from matplotlib import pyplot as plt 
 from datetime import datetime
 import numpy as np  
@@ -9,30 +8,30 @@ DOUBLE_ZERO = '00'
 BLACK = 'BLACK'
 RED = 'RED'
 
-class roulette_martinagle:
-    def __init__(self,starting_units=100):
-        self.starting_units = starting_units
-        self.bet_target = BLACK
-        self.current_units = starting_units
-        self.spin_history = []
 
-        # spinner initialization
-        self.spinner_array = []
+class roulette_martinagle:
+    def __init__(self,bet_target=BLACK,starting_units=100):
+        self.starting_units = starting_units
+        self.bet_target = bet_target
+        self.current_units = starting_units
+        self.spin_count = 0
+
+        self.spinner = []
         for _ in range(49):
-            self.spinner_array.append(BLACK)
-            self.spinner_array.append(RED)
-        self.spinner_array.append(ZERO)
-        self.spinner_array.append(DOUBLE_ZERO)
+            self.spinner.append(BLACK)
+            self.spinner.append(RED)
+        self.spinner.append(ZERO)
+        self.spinner.append(DOUBLE_ZERO)
 
     def spin(self):
         random.seed(datetime.now())
         random_num = random.randrange(0, 100)
-        spin_value = self.spinner_array[random_num]
+        spin_value = self.spinner[random_num]
         result = []
         if spin_value in [ZERO, DOUBLE_ZERO]:
             result = spin_value
         result = [spin_value, random_num]
-        self.spin_history.append(result)
+        self.spin_count += 1
         return result
 
     def bet(self,units_bet):
@@ -41,12 +40,11 @@ class roulette_martinagle:
         if self.bet_target in spin_outcome:
             units_won = 2 * units_bet
             self.current_units += units_won
-            return True
-        else:
-            return False
+            return True  # Win
+        return False  # Lose
 
     def reset(self):
-        self.spin_history = []
+        self.spin_count = 0
         self.current_units = self.starting_units
 
     def play_to_bust(self):
@@ -54,23 +52,41 @@ class roulette_martinagle:
         while bet_units <= self.current_units:
             won_bet = self.bet(bet_units)
             if won_bet:
+                # Martingale strategy dictates that a bet after a win is the minimum bet value
                 bet_units = 1
             else:
+                # Martingale strategy dictates that a bet after a loss is double the previous bet's value
                 bet_units = (2 * bet_units)
-        number_of_session_spins = len(self.spin_history)
+        # Now the player has gone bust due to their inability to win back their losses
+        spin_count = self.spin_count
         self.reset()
-        return number_of_session_spins
+        return spin_count
 
-history = []
-martingale = roulette_martinagle()
-for i in range(10000):
-    number_of_spins = martingale.play_to_bust()
-    history.append(number_of_spins)
 
-a = np.array(history)
-bins = []
-for i in range(0, 500, 10):
-    bins.append(i)
-plt.hist(a, bins = bins) 
-plt.title("Martingale - Spins to Bust") 
-plt.show()
+def get_stats(sample_size):
+    stats = []
+    martingale = roulette_martinagle()
+    for _ in range(sample_size):
+        number_of_spins = martingale.play_to_bust()
+        stats.append(number_of_spins)
+    return stats
+
+
+def visualize_stats(stats):
+    a = np.array(stats)
+    bins = []
+    for i in range(0, 1000, 10):
+        bins.append(i)
+    plt.hist(a, bins = bins) 
+    plt.title("Martingale Roulette - Spin Number that Bust - %s Sessions" % len(stats)) 
+    plt.show()
+
+
+def main():
+    SAMPLE_SIZE = 10000  # Number of Martingale sessions - A session ends when the player can no longer win back their losses
+    stats = get_stats(SAMPLE_SIZE)
+    visualize_stats(stats)
+
+
+if __name__ == '__main__':
+    main()
